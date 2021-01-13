6afc04d9ece53fabae11dce397787a27d9bb2037
Sampsoni <sami.repo@live.com>
2021-01-13 16:54:44 +0200

    Initial Test Script for testing VSCode Debugging.

---
+++ Assets/Scripts/InitialTestScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialTestScript : MonoBehaviour
+{
+    // TODO - TESTING TESTING!
+
+    private void Start()
+    {
+        Debug.Log("ToimMiiiiMiiiiMiiii!");
+    }
+}
