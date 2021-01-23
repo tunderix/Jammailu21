2f2391c825612c3146465459ec4b6cfde68215ec
Showerduck <showerduck900@gmail.com>
2021-01-23 22:49:10 +0200

    Created VFX playground scene

--- Assets/Scripts/AnttiTesti.cs
+++ Assets/Scripts/AnttiTesti.cs
@@ -9,14 +9,26 @@ namespace TerraFirma
         // Start is called before the first frame update
         void Start()
         {
-        
+            Debug.Log("update");
+
+
+
+
+
+
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            Debug.Log("update");
+
+
+
+
+
         }
+
 
     }
 }
