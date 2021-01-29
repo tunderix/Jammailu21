aee0a8132b2df2bc9c737cad93e12dac80b7967c
Sampsoni <sami.repo@live.com>
2021-01-29 11:58:40 +0200

    Added Inspiration system classes.

---
+++ Assets/Scripts/InspirationSystem/InspirationResponder.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TerraFirma.InspirationSystem
+{
+    public class InspirationResponder : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/InspirationSystem/InspirationTrigger.cs
@@ -0,0 +1,11 @@
+using UnityEngine; 
+
+namespace TerraFirma.InspirationSystem
+{
+    public class InspirationTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other) {
+            
+        }
+    }
+}
