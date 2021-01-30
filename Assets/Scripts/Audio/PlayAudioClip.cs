9cf390326ac7e996ea6de1b610b5e6d0cedd3211
Ramseskus <61796422+Ramseskus@users.noreply.github.com>
2021-01-30 21:24:05 +0200

    AudioScripts
    Added audio script for Main Theme and Effects

---
+++ Assets/Scripts/Audio/PlayAudioClip.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TerraFirma
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class PlayAudioClip : MonoBehaviour
+    {
+        public AudioClip clip;
+        [SerializeField] private AudioSource audioData;
+
+        [Range(0.0f, 1.0f)]
+        [SerializeField] private float volumeSlider = 0.5f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                other.gameObject.SetActive(true);
+                playClip();
+            }
+        }
+        void playClip()
+        {
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+        }
+
+        void OnGUI()
+        {
+            volumeSlider = GUI.HorizontalSlider(new Rect(25, 25, 100, 60), volumeSlider, 0.0F, 1.0F);
+            audioData.volume = volumeSlider;
+        }
+
+
+    }
+}
