696ce4113d5384447446f4f4d4a99e1560a47f1c
Sampsoni <sami.repo@live.com>
2021-01-31 01:30:36 +0200

    Added stepping to ship

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -7,6 +7,7 @@ namespace TerraFirma
     {
         [SerializeField] float speed;
         [SerializeField] float decelerationModifier;
+        private float maxSpeed;
 
 
         private bool anchorIsDown;
@@ -14,7 +15,7 @@ namespace TerraFirma
         private void Start()
         {
             anchorIsDown = false;
-
+            maxSpeed = speed;
         }
 
         private void Update()
@@ -23,9 +24,19 @@ namespace TerraFirma
             {
                 speed -= 0.5f * decelerationModifier * Time.deltaTime;
             }
+            else
+            {
+                speed += 0.5f * decelerationModifier * Time.deltaTime;
+                if (speed > maxSpeed) speed = maxSpeed;
+            }
+
             if (speed <= 0f)
             {
                 speed = 0f;
+                if (Input.GetKeyDown(KeyCode.X))
+                {
+                    anchorIsDown = false;
+                }
             }
         }
 
