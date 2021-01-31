ca03abeee1ffce3f6eac53e3c71add7fd94bde11
Ramseskus <61796422+Ramseskus@users.noreply.github.com>
2021-01-31 12:51:52 +0200

    MainTheme
    added main theme audio and volume slider

--- Assets/Scripts/Audio/VolumeController.cs
+++ Assets/Scripts/Audio/VolumeController.cs
@@ -11,6 +11,9 @@ namespace TerraFirma
 
         [Range(0.0f, 1.0f)]
         [SerializeField] private float volumeSlider;
+
+        private string volumeSliderLabel = "MUSIC";
+        private GUIContent volumeLabel;
         private void Start()
         {
             playAudioClip = new PlayAudioClip();
@@ -23,6 +26,7 @@ namespace TerraFirma
         {
             volumeSlider = GUI.HorizontalSlider(new Rect(25, 25, 200, 60), volumeSlider, 0.0F, 1.0F);
             audioData.volume = volumeSlider;
+            GUI.Label(new Rect(30, 5, 200, 60), volumeSliderLabel);
         }
     }
 }
